f61aea6f32b1ca058622d40a8393295f970a54e9
Dave Horton <daveh@beachdognet.com>
2013-06-08 21:53:00 -0400

    initial checkin

---
+++ fs-message.cpp
@@ -0,0 +1,142 @@
+//
+//  fs-message.cpp
+//  ssp
+//
+//  Created by Dave Horton on 6/8/13.
+//  Copyright (c) 2013 Beachdog Networks. All rights reserved.
+//
+#include <boost/tokenizer.hpp>
+
+#include "fs-message.h"
+#include "ssp-controller.h"
+
+namespace ssp {
+    FsMessage::FsMessage( const string& data ) : m_bValid(false), m_bComplete(false) {
+        reset() ;
+        try {
+            parse( data ) ;
+            m_bValid = true ;
+        } catch( exception& e ) {
+            SSP_LOG(log_error) << "Error parsing freeswitch msg: " << data << ": " << e.what() << endl ;
+        }
+    }
+    FsMessage::~FsMessage() {
+        
+    }
+    
+    void FsMessage::reset() {
+        m_category = unknown_category ;
+        m_type = unknown_type ;
+        m_replyStatus = undefined ;
+        m_rawMsg.clear() ;
+        m_strContent.clear() ;
+        m_strReplyText.clear() ;
+        m_nContentLength = 0 ;
+    }
+    
+    void FsMessage::parse( const string& data ) throw(string) {
+        m_rawMsg.append( data ) ;
+        
+        boost::char_separator<char> sep("\r\n") ;
+        tokenizer tok( m_rawMsg, sep) ;
+        int i = 0 ;
+        for( tokenizer::iterator it = tok.begin(); it != tok.end(); ++i, ++it ) {
+            const string& line = *it ;
+            if( unknown_category == m_category ) {
+                if( 0 != line.find("Content-Type: ")) throw("Invalid msg: Expected Content-Type on first line: " + line) ;
+                
+                string s = line.substr(14) ;
+                tokenizer tok2(s, boost::char_separator<char>("/") ) ;
+                if( 2 != distance(tok2.begin(), tok2.end()) ) throw("Invalid Content-Type: " + s) ;
+                
+                tokenizer::iterator it2 = tok2.begin() ;
+                if( 0 == (*it2).compare("api")) m_category = api ;
+                else if( 0 == (*it2).compare("auth")) m_category = auth ;
+                else if( 0 == (*it2).compare("command")) m_category = command ;
+                else if( 0 == (*it2).compare("text")) m_category = text ;
+                else throw("Invalid/unknown Content-Type: '" + line + "': " + (*it2) + " is an unknown category") ;
+                
+                ++it2 ;
+                if( 0 == (*it2).compare("reply") )  m_type = reply ;
+                else if( 0 == (*it2).compare("request") )  m_type = request ;
+                else if( 0 == (*it2).compare("response") )  m_type = response ;
+                else if( 0 == (*it2).compare("disconnect-notice") )  m_type = disconnect_notice ;
+                else throw("Invalid/unknown Content-Type: '" + line + "': " + (*it2) + " is an unknown type") ;
+            }
+            else if( command == m_category && reply == m_type ) {
+                if( undefined != m_replyStatus ) throw("Invalid msg: multiple lines of Reply-Text: " + line) ;
+                if( 0 != line.find("Reply-Text: ")) throw("Invalid msg: Expected Reply-Text on second line of command/reply msg: " + line) ;
+                m_strReplyText = line ;
+                string s = line.substr(12) ;
+                if( 0 == s.find("+OK") ) m_replyStatus = ok ;
+                else if( 0 == s.find("+ERR")) m_replyStatus = err ;
+                else throw("Invalid reply status: " + s ) ;
+            }
+            else if( (api == m_category && response == m_type) || text == m_category ) {
+                if( 0 == m_nContentLength ) {
+                    if( 0 != line.find("Content-Length: ")) throw("Invalid msg: Expected Content-Length on second line of api/response or text msg: " + line) ;
+                    string s = line.substr(16) ;
+                    m_nContentLength = atoi( s.c_str() ) ;                    
+                }
+                else {
+                    m_strContent.append( line ) ;
+                }
+            }
+        }
+        
+        if( m_nContentLength > 0 && m_strContent.length() != m_nContentLength ) {
+            ostringstream reportedLength, actualLength ;
+            reportedLength << m_nContentLength ;
+            actualLength << m_strContent.length() ;
+            throw("Content-Length was " + reportedLength.str() + " but read " + actualLength.str() ) ;
+        }
+        
+        m_bValid = true ;
+        m_bComplete = true ;
+        
+    }
+    
+    bool FsMessage::getSipProfile( const string& profile, string& address, unsigned int port ) const {
+        
+        tokenizer tok( m_strContent, boost::char_separator<char>("\r\n")) ;
+        for( tokenizer::iterator it = tok.begin(); it != tok.end(); ++it ) {
+            tokenizer tok2( *it, boost::char_separator<char>(" \t")) ;
+            tokenizer::iterator it2 = tok2.begin() ;
+            if( 0 == (*it2).compare( profile ) ) {
+                advance( it2, 2 ) ;
+                const string& contact = *it2 ;
+                tokenizer tok3( *it2,  boost::char_separator<char>("@:")) ;
+                tokenizer::iterator it3 = tok3.begin() ;
+                advance(it3, 2) ;
+                address = *it3 ;
+                if( ++it3 != tok3.end() ) port = atoi( (*it3).c_str() ) ;
+                else port = 5060 ;
+                return true ;
+            }
+        }
+        
+        return false ;
+    }
+    bool FsMessage::getFsStatus( unsigned int& nCurrentSessions, unsigned int& nMaxSessions ) const {
+        tokenizer tok( m_strContent, boost::char_separator<char>("\r\n")) ;
+        tokenizer::iterator it = tok.begin();
+        
+        if( distance( tok.begin(), tok.end() ) < 5 ) return false ;
+        advance( it, 3 ) ;
+        if( !parseLeadingInteger(*it, nCurrentSessions) ) return false ;
+        advance( it, 1 ) ;
+        if( !parseLeadingInteger(*it, nMaxSessions) ) return false ;
+        
+        return true ;
+    }
+    bool FsMessage::parseLeadingInteger( const string& str, unsigned int& number) const {
+        tokenizer tok( str, boost::char_separator<char>(" ")) ;
+        const string& s = *(tok.begin());
+        if( !s.empty() && std::find_if(s.begin(), s.end(), ::isdigit ) == s.end() ) {
+            number = ::atoi( s.c_str() ) ;
+            return true ;
+        }
+        return false ;
+    }
+
+}
---
+++ fs-message.h
@@ -0,0 +1,86 @@
+//
+//  fs-message.h
+//  ssp
+//
+//  Created by Dave Horton on 6/8/13.
+//  Copyright (c) 2013 Beachdog Networks. All rights reserved.
+//
+
+#ifndef __ssp__fs_message__
+#define __ssp__fs_message__
+
+#include <iostream>
+
+#include "ssp.h"
+
+using namespace std ;
+
+namespace ssp {
+    class FsMessage {
+    public:
+        FsMessage( const string& data ) ;
+        ~FsMessage() ;
+ 
+        enum FS_MSG_CATEGORY {
+            unknown_category,
+            api,
+            auth,
+            command,
+            text
+        } ;
+        
+        enum FS_MSG_TYPE {
+            unknown_type,
+            request,
+            reply,
+            response,
+            disconnect_notice
+        } ;
+        
+        enum FS_REPLY_STATUS {
+            undefined,
+            ok,
+            err
+        } ;
+
+        bool isValid() const { return m_bValid; }
+        bool isComplete() const { return m_bComplete; }
+        FS_MSG_CATEGORY getCategory() const { return m_category ; }
+        FS_MSG_TYPE getType() const { return m_type ; }
+        bool getReplyText( string& strReplyText ) {
+            if( 0 == m_strReplyText.length() ) return false ;
+            strReplyText = m_strReplyText ;
+            return true ;
+        }
+        bool getContent( string& strContent ) {
+            if( 0 == m_nContentLength ) return false ;
+            strContent = m_strContent ;
+            return true ;
+        }
+        FS_REPLY_STATUS getReplyStatus() const { return m_replyStatus; }
+        
+        bool getSipProfile( const string& profile, string& address, unsigned int port ) const ;
+        bool getFsStatus( unsigned int& nCurrentSessions, unsigned int& nMaxSessions ) const ;
+        
+        
+    protected:
+        void reset() ;
+        void parse( const string& data ) throw(string) ;
+        
+    private:
+        FsMessage() ;
+        
+        bool parseLeadingInteger( const string& s, unsigned int& number) const ;
+        
+        FS_MSG_CATEGORY m_category ;
+        FS_MSG_TYPE     m_type ;
+        FS_REPLY_STATUS m_replyStatus ;
+        string          m_strReplyText ;
+        unsigned int    m_nContentLength ;
+        string          m_strContent ;
+        string          m_rawMsg ;
+        bool            m_bValid ;
+        bool            m_bComplete ;
+    } ;
+}
+#endif /* defined(__ssp__fs_message__) */
