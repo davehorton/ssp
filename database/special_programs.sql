9ca7570a603239b33fd79fad31cec9e04a15029c
Dave Horton <daveh@beachdognet.com>
2014-11-16 09:39:24 -0500

    minor time zone diffs in reports

--- database/special_programs.sql
+++ database/special_programs.sql
@@ -6,8 +6,8 @@ left outer join
 select cpn.calling_program_id as program_id, count(*) as calls_received 
 from calling_program_number cpn, cdr_session cdr
 where cdr.called_party_number_in = cpn.called_number_in
-and DATE_SUB(cdr.start_time,INTERVAL 5 HOUR) > DATE_SUB(CURDATE(),INTERVAL 1 DAY)
-and DATE_SUB(cdr.start_time,INTERVAL 5 HOUR) < CURDATE()
+and DATE_SUB(cdr.start_time,INTERVAL 4 HOUR) > DATE_SUB(CURDATE(),INTERVAL 1 DAY)
+and DATE_SUB(cdr.start_time,INTERVAL 4 HOUR) < CURDATE()
 group by 1
 ) as calls
 ON calls.program_id = cp.id 
